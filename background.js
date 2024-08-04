62577a9341965a42625c0ad2139397a3618c73fb
Avinash Shukla <avinashukla2000@gmail.com>
2024-08-04 17:35:23 +0530

    feat(blur : add ability to blur content): Blur the whatsapp Content for more Privacy

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed!");
-});
-
-// Example: Listening for a message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "hello") {
-        sendResponse({ message: "hello from background" });
-    }
+    console.log("Extension installed.");
 });
